f129267f8e71aef35945802d8d2e1161c5974e8c
Konrad Pieczynski <konrad.pieczynski@gmail.com>
2023-09-18 11:33:01 +0200

    using spring 3.1. Rewritten course code - removed depracted functions from 2.7 and added new recommended ways from spring 3.0+

---
+++ src/main/java/com/kodilla/library/LibrarySecurityApplication.java
@@ -0,0 +1,13 @@
+package com.kodilla.library;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibrarySecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LibrarySecurityApplication.class, args);
+    }
+
+}
